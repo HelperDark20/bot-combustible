0fe273cfda60ee6a754d81d597f44d0603ab6556
Carlos <toxicsells@gmail.com>
2026-05-07 08:21:20 -0500

    test

--- config.py
+++ config.py
@@ -26,3 +26,13 @@ PORT = int(
 client = OpenAI(
     api_key=OPENAI_API_KEY
 )
+
+# ==========================================
+# CONFIG COMBUSTIBLE DEFAULT
+# ==========================================
+
+DEFAULT_KM_L = 42.0
+
+DEFAULT_VALOR_GALON = 17000
+
+DEFAULT_TANQUE = 0
